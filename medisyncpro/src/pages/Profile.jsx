0417e733ac627c7a410366afd98aa281d85371c2
drilonsaiti <drilon-saiti@hotmail.com>
2024-02-17 15:07:30 +0100

    Bug fixes.

--- medisyncpro/src/pages/Profile.jsx
+++ medisyncpro/src/pages/Profile.jsx
@@ -3,8 +3,8 @@ import {useGetRole} from "../services/useGetRole.js";
 import PatientDetailsProfile from "../features/patient/PatientDetailsProfile.jsx";
 import Spinner from "../ui/Spinner.jsx";
 import ClinicProfileDetails from "../features/clinic/ClinicProfileDetails.jsx";
-import ReceptionistDetailsProfile from "../features/Receptionist/ReceptionistDetailsProfile.jsx";
-import DoctorProfileDetails from "../features/Doctor/DoctorProfileDetails.jsx";
+import ReceptionistDetailsProfile from "../features/receptionist/ReceptionistDetailsProfile.jsx";
+import DoctorProfileDetails from "../features/Doctor/doctorProfileDetails.jsx";
 
 const Profile = () => {
     const {roles, isLoading: isLoadingRole} = useGetRole();
